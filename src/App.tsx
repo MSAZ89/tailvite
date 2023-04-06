2bc9400b1feb63940a751f0734b2def229e373f1
Michael Simon AZ <michaelsimonaz@gmail.com>
2023-04-06 12:09:41 -0700

    initial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen p-4 bg-slate-200">
+      <h1 className="text-slate-200 bg-black py-4 px-12 text-2xl font-bold">
+        App
+      </h1>
+    </div>
+  );
+}
+
+export default App;
